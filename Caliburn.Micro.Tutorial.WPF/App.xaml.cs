793c094fecff0a97146536e782e1ffc9dd5fd28c
Alex Kondratenko <moneyaxis@gmail.com>
2023-12-23 17:25:27 +0200

    Initial commit

---
+++ Caliburn.Micro.Tutorial.WPF/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Caliburn.Micro.Tutorial.WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ Caliburn.Micro.Tutorial.WPF/Bootstrapper.cs
@@ -0,0 +1,24 @@
+﻿using Caliburn.Micro.Tutorial.WPF.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Caliburn.Micro.Tutorial.WPF
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            LogManager.GetLog = type => new DebugLog(type);
+            Initialize();
+        }
+
+        protected override async void OnStartup(object sender, StartupEventArgs e)
+        {
+            await DisplayRootViewForAsync(typeof(ShellViewModel));
+        }
+    }
+}

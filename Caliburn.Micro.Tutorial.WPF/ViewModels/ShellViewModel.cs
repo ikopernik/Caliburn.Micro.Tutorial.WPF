deae7857615a20f46d07b0ef676338efaf133a36
Alex Kondratenko <moneyaxis@gmail.com>
2023-12-23 17:58:37 +0200

    Add Category View

--- Caliburn.Micro.Tutorial.WPF/ViewModels/ShellViewModel.cs
+++ Caliburn.Micro.Tutorial.WPF/ViewModels/ShellViewModel.cs
@@ -8,5 +8,16 @@ namespace Caliburn.Micro.Tutorial.WPF.ViewModels
 {
     public class ShellViewModel: Conductor<object>
     {
+        public Task EditCategories()
+        {
+            var viewmodel = IoC.Get<CategoryViewModel>();
+            return ActivateItemAsync(viewmodel, new CancellationToken());
+        }
+
+        protected async override void OnViewLoaded(object view)
+        {
+            base.OnViewLoaded(view);
+            await EditCategories();
+        }
     }
 }
